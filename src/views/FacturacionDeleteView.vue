1f56a170c20a11197b48ca09e9e526e25eb93204
jaimesRodriguez21 <jaimesrodriguezjg@gmail.com>
2023-01-23 14:39:23 -0500

    first commit

---
+++ src/views/FacturacionDeleteView.vue
@@ -0,0 +1,25 @@
+<template >
+    <div class="container">
+        <b-form-input v-model="text" placeholder="Enter your name"></b-form-input>
+        <div class="mt-2">Value: {{ text }}</div>
+        <b-button  v-on:click="eliminarFactura()" variant="danger">Eliminar </b-button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text: ''
+        }
+    },
+    methods: {
+        eliminarFactura() {
+           this.axios.delete(`http://localhost:8084/facturacionWS/api/facturacion/${this.text}`).then(()=>{
+             alert(`Factura #${this.text} eliminada`)
+            });
+        }
+
+    }
+}
+</script>
---
+++ src/views/FacturcionView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <b-table striped hover :items="facturas"></b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      facturas: Array()
+    }
+  },
+  created() {
+    this.verFactura();
+  },
+  methods: {
+    verFactura() {
+
+      const api = "http://localhost:8084/facturacionWS/api/facturacion/list";
+      this.axios.get(api).then((response) => {
+        response.data.forEach(element => {
+          let factura = {
+            id: element.numerofactura,
+            nombre: element.nombreCliente,
+            iva: element.iva,
+            subtotal: element.subTotal,
+            total: element.total
+          }
+          this.facturas.push(factura);
+        });
+      })
+    },
+
+  }
+}
+
+
+
+</script>
